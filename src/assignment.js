43af4223a5507dc604064d201bed9f907453942a
Oluwafisayomi Adebowale <61998031+the-yomi@users.noreply.github.com>
2022-07-12 19:41:49 +0000

    Attempted challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+    for(number of arrayOfNumbers){
+        sum += number
+    }    
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count =0
+  for(number of arrayOfNumbers){
+    if (number%2 == 0) {
+      count++   
+    }
+  }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
